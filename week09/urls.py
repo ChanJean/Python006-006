7aae43ebbe640455d3489bae086962d3a979a88e
ChanJean <34547785+ChanJean@users.noreply.github.com>
2021-02-27 21:16:12 +0800

    Add files via upload

--- week09/urls.py
+++ week09/urls.py
@@ -1,18 +1,28 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.documentation import include_docs_urls
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+from django.urls import path
+from .views import OrdersViewSet, CreateOrdersView
+
+orders_list = OrdersViewSet.as_view({
+    'get': 'list'
+})
+
+orders_detail = OrdersViewSet.as_view({
+    'get': 'retrieve',
+})
+
+# orders_create = OrdersViewSet.as_view({
+#     'post': 'create',
+# })
+
+orders_cancel = OrdersViewSet.as_view({
+    'get': 'cancel'
+})
 
 urlpatterns = [
-    path('docs/', include_docs_urls(title='commerce')),
-    path('orders', include('orders.extend_urls')),
-    path('orders/', include('orders.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('', orders_list, name='orders-list'),
+    path('<int:pk>', orders_detail, name='orders-detail'),
+    # path('create', orders_create, name='orders-create'),
+    path('create', CreateOrdersView.as_view(), name='orders-create'),
+    path('<int:pk>/cancel', orders_cancel, name='orders-cancel')
 ]
+
+
